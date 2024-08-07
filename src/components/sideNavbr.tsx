754b1d12ad722c802190dffec9638be05e94bfce
eaglementality <jesseowusuyeboah99@gmail.com>
2024-08-07 11:40:09 +0000

    update

--- src/components/sideNavbr.tsx
+++ src/components/sideNavbr.tsx
@@ -1,7 +1,22 @@
 function Navbar() {
   return (
-    <main className="absolute top-0 bottom-0 left-0 border h-full p-5">
-      <div className="p-5 border mb-10 mt-5">User Name</div>
+    <main className="absolute top-0 bottom-0 left-0 border h-full p-5 px-20">
+      <div className="p-5 border mt-10">User Name</div>
+      <div className="my-20 grid space-y-6">
+        <span>Dashboard</span>
+        <span>My Courses</span>
+        <span>Classroom</span>
+        <span>AI Tutor</span>
+        <span>Practice Modules</span>
+        <span>Settings</span>
+      </div>
+      <div className="grid space-y-8">
+        <hr />
+        <span>Study Rank</span>
+        <hr />
+        <span>Your Achievements</span>
+        <hr />
+      </div>
     </main>
   );
 }
